eac9ac237a2baf5b5998102638713857c04c940f
Will Richards <richardsw2017@gmail.com>
2020-09-02 22:53:10 -0600

    adding part info page using route params

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router';
 
 import HomePage from '@/home/HomePage.vue';
 import RoboBuilder from '@/build-bot/RoboBuilder.vue';
+import PartInfo from '@/parts/PartInfo.vue';
 
 Vue.use(Router);
 
@@ -15,5 +16,9 @@ export default new Router({
     path: '/build',
     name: 'Build',
     component: RoboBuilder,
+  }, {
+    path: '/parts/:partType/:id',
+    name: 'Parts',
+    component: PartInfo,
   }],
 });
