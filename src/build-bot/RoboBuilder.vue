61ad463583ef4e40175f9d692c01a7c035080228
Will Richards <richardsw2017@gmail.com>
2020-12-12 23:34:57 -0700

    dispatching actions to save cart data to api

--- src/build-bot/RoboBuilder.vue
+++ src/build-bot/RoboBuilder.vue
@@ -112,7 +112,7 @@ export default {
         + robot.rightArm.cost
         + robot.torso.cost
         + robot.base.cost;
-      this.$store.commit('addRobotToCart', { ...robot, cost });
+      this.$store.dispatch('addRobotToCart', { ...robot, cost });
       this.addedToCart = true;
     },
   },
