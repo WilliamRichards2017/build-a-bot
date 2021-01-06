f4e27c286c1515a1379df99d20ca49c848e9add2
Will Richards <richardsw2017@gmail.com>
2021-01-06 13:05:43 -0700

    adding namespaces to vuex store modules

--- src/build-bot/RoboBuilder.vue
+++ src/build-bot/RoboBuilder.vue
@@ -64,7 +64,7 @@ import CollapsibleSection from '../shared/CollapsibleSection.vue';
 export default {
   name: 'RobotBuilder',
   created() {
-    this.$store.dispatch('getParts');
+    this.$store.dispatch('robots/getParts');
   },
   beforeRouteLeave(to, from, next) {
     if (this.addedToCart) {
@@ -112,7 +112,7 @@ export default {
         + robot.rightArm.cost
         + robot.torso.cost
         + robot.base.cost;
-      this.$store.dispatch('addRobotToCart', { ...robot, cost })
+      this.$store.dispatch('robots/addRobotToCart', { ...robot, cost })
         .then(() => this.$router.push('/cart'));
       this.addedToCart = true;
     },
