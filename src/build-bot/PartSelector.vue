9c7288f28e9826fa543ecd621594e13f4681d7eb
Will Richards <richardsw2017@gmail.com>
2021-01-06 23:51:43 -0700

    adding custom directives

--- src/build-bot/PartSelector.vue
+++ src/build-bot/PartSelector.vue
@@ -1,4 +1,4 @@
-<template>
+<template xmlns:v-pin="http://www.w3.org/1999/xhtml">
   <div class="part" :class="position">
     <router-link :to="{
         name: 'Parts',
@@ -10,11 +10,14 @@
     </router-link>
     <button @click="selectPreviousPart()" class="prev-selector"></button>
     <button @click="selectNextPart()" class="next-selector"></button>
-    <span class="sale" v-show="selectedPart.onSale">Sale!</span>
+    <span v-pin="{ top: '10px', right: '10px'}" class="sale"
+          v-show="selectedPart.onSale">Sale!</span>
   </div>
 </template>
 
 <script>
+import pinDirective from '../shared/pin-directive';
+
 function getPreviousValidIndex(index, length) {
   const deprecatedIndex = index - 1;
   return deprecatedIndex < 0 ? length - 1 : deprecatedIndex;
@@ -26,6 +29,7 @@ function getNextValidIndex(index, length) {
 }
 
 export default {
+  directives: { pin: pinDirective },
   props: {
     parts: { type: Array, required: true },
     position: {
@@ -81,9 +85,6 @@ export default {
   border: 3px solid #aaa;
 }
 .sale {
-  position: absolute;
-  bottom: 5px;
-  right: 5px;
   color: white;
   background-color: red;
   padding: 3px;
---
+++ src/shared/pin-directive.js
@@ -0,0 +1,10 @@
+/* eslint no-param-reassign: "error" */
+
+export default {
+  bind: (element, binding) => {
+    Object.keys(binding.value).forEach((position) => {
+      element.style[position] = binding.value[position];
+    });
+    element.style.position = 'absolute';
+  },
+};
